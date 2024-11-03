8b0655503ac0a18b817bc03bfd1a080c687e9e41
Malov Alexey <alexkkk@inbox.ru>
2024-11-03 21:09:32 +0700

    Очередь на кольцевом массиве

--- data_structures/my_queue.ts
+++ data_structures/my_queue.ts
@@ -24,10 +24,63 @@ class ArrayQueue<T> {
     }
 }
 
-const arrayQueue = new ArrayQueue()
+class RingArrayQueue<T> {
+    private queue: (T | null)[]
 
+    private left: number = 0
+    private right: number = 0
+
+    constructor (ringArrayLength: number = 10) {
+        this.queue = new Array(ringArrayLength)
+    }
+
+    size(): number {
+        return this.queue.length
+    }
+
+    enqueue(data: T) {
+        if (this.right === this.size()) {
+            this.right = 0
+            this.left = this.size() - 1
+        }
+        this.queue[this.right] = data
+        this.right ++
+    }
+
+    dequeue(): T | null {
+        if (this.left === this.size()) {
+            this.left = 0
+        }
+        const item = this.queue[this.left]
+        this.queue[this.left] = null
+        this.left ++
+        return item
+    }
+
+    traverse() {
+       console.log(this.queue) 
+    }
+}
+
+const arrayQueue = new ArrayQueue()
 arrayQueue.enqueue('Hello')
 arrayQueue.enqueue('World')
 arrayQueue.traverse()
 console.log(`Вышел '${arrayQueue.dequeue()}'`)
 arrayQueue.traverse()
+
+const ringArrayQueue = new RingArrayQueue(3)
+ringArrayQueue.enqueue('Hello')
+ringArrayQueue.enqueue('I am')
+ringArrayQueue.enqueue('L3XxXa')
+ringArrayQueue.traverse()
+console.log(`Вышел '${ringArrayQueue.dequeue()}'`)
+ringArrayQueue.traverse()
+ringArrayQueue.enqueue('Overflowed')
+ringArrayQueue.traverse()
+ringArrayQueue.enqueue('Overlowed 2')
+ringArrayQueue.traverse()
+console.log(ringArrayQueue.dequeue())
+ringArrayQueue.traverse()
+ringArrayQueue.enqueue('Overflowed 3')
+ringArrayQueue.traverse()
