a46a571328a421327ad81b5ff1a7131f34c02ddb
Malov Alexey <alexkkk@inbox.ru>
2024-11-04 12:57:32 +0700

    Размещение дипломов на стене

---
+++ classic_algos/binary_search_find_by_answer.ts
@@ -0,0 +1,15 @@
+function binarySearch(w: number, h: number, n: number) {
+    let l = Math.max(w, h)
+    let r = l * n
+
+    while (l + 1 < r) {
+        const middle = Math.floor((l + r) / 2)
+        const res = (middle / w) * (middle / h)
+        if (res < n) {
+            l = middle
+        } else if (res > n) {
+            r = middle
+        }
+    }
+    return r
+}
