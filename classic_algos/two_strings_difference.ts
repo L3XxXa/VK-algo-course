cdc851e9d42323688410504823340ed182dfc0f8
Malov Alexey <alexkkk@inbox.ru>
2024-11-10 12:36:25 +0700

    Вывести различие между строками (переделанная задача)

--- classic_algos/two_strings_difference.ts
+++ classic_algos/two_strings_difference.ts
@@ -1,23 +1,56 @@
+/** 
+ * Time: O(3 * N)
+ */
 function twoStringDifference(a: string, b: string) {
-    const map = new Map<string, number>([])
-    for (let i of b) {
-        if (map.has(i)) {
-            map[i] += 1
-        } else {
-            map[i] = 1
+    const stringBMap: Map<string, number> = new Map()
+
+    for (const i of b) {
+        const curr = stringBMap.get(i) ?? 0
+        stringBMap.set(i, curr + 1)
+    }
+
+    for (const i of a) {
+        const curr = stringBMap.get(i)
+        if (curr) {
+            stringBMap.set(i, curr - 1)
         }
     }
-    for (let i of a) {
-        if (map.has[i]) {
-            map[i] -= 1
-        }
-    }
-    for (let i of map.keys()) {
-        if (map[i] > 0) {
-            return i
+
+    for (const item of stringBMap.keys()) {
+        if (stringBMap.get(item)) {
+            return item
         }
     }
     return null
 }
 
-console.log(twoStringDifference('asd', 'bsd'))
+/**
+ * Time: O(2 * N)
+ */
+function twoStringDifferenceOptimized(a: string, b: string) {
+    const stringAMap: Map<string, number> = new Map()
+
+    for (const i of a) {
+        const curr = stringAMap.get(i) ?? 0
+        stringAMap.set(i, curr + 1)
+    }
+
+    for (const i of b) {
+        const curr = stringAMap.get(i)
+        if (curr) {
+            stringAMap.set(i, curr - 1)
+            if (curr - 1 === 0) {
+                stringAMap.delete(i)
+                continue
+            }
+            continue
+        }
+        return i
+    }
+    return null
+}
+
+console.log(twoStringDifference('abacaba', 'abababa'))
+console.log(twoStringDifferenceOptimized('abacaba', 'abababa'))
+
+  
