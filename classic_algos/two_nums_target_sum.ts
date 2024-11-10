56845c9c3d858b4956090a55c19871773634b70a
Malov Alexey <alexkkk@inbox.ru>
2024-11-10 12:55:44 +0700

    Поиск суммы в неотсортированном массиве

--- classic_algos/two_nums_target_sum.ts
+++ classic_algos/two_nums_target_sum.ts
@@ -12,4 +12,22 @@ function twoElementsSumNotSorted(values: number[], target: number): number[] | n
     return null
 }
 
+function twoElementsSumSorted(arr: number[], target: number): number[] | null {
+    let l = 0
+    let r = arr.length - 1
+    while (l <= r) {
+        const sum = arr[l] + arr[r]
+        if (sum === target) {
+            return [l, r]
+        } else if (sum < target) {
+            l += 1
+        } else {
+            r -= 1
+        }
+    }
+    return null
+}
+
+console.log(twoElementsSumSorted([1, 2, 3, 4, 5, 6, 7, 8, 9], 13))
+
 console.log(twoElementsSumNotSorted([1, 2, 3, 4, 5, 1, 54, 2], 59))
