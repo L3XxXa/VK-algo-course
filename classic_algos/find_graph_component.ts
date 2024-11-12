c962a61319a6dfda951644f9c88769eae54446a3
Malov Alexey <alexkkk@inbox.ru>
2024-11-12 23:18:08 +0700

    Раскраска графа

--- classic_algos/find_graph_component.ts
+++ classic_algos/find_graph_component.ts
@@ -24,6 +24,31 @@ function findComponents(graph: Map<string, string[]>, startVertice: string) {
     return connectedComponents
 }
 
+function dfsColor(graph: Map<string, string[]>, startVertice: string, colors: Map<string, number>, color: number) {
+    colors.set(startVertice, color)
+    for (const neighbour of graph.get(startVertice)) {
+        if (colors.get(neighbour) === 0) {
+            dfsColor(graph, neighbour, colors, color)
+        }
+    }
+}
+
+function colorGraph(graph: Map<string, string[]>, startVertice: string) {
+    const colors = new Map<string, number>()
+    for (const vertice of graph.keys()) {
+        colors.set(vertice, 0)
+    }
+    let color = 0
+    for (const vertice of graph.keys()) {
+        if (colors.get(vertice) === 0) {
+            color++;
+            const component: string[] = []
+            dfsColor(graph, vertice, colors, color)
+        }
+    }
+    return colors
+}
+
 const vertices = new Map<string, string[]>()
 vertices.set('A', ['B', 'C'])
 vertices.set('B', ['D', 'E'])
@@ -35,3 +60,4 @@ vertices.set('G', ['E'])
 vertices.set('O', [])
 
 console.log(findComponents(vertices, 'A'))
+console.log(colorGraph(vertices, 'A'))
